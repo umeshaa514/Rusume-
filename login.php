4e132e49d091d27ea042b765259f99b9608e6948
umeshaa514 <ur4190150@gmail.com>
2025-04-02 22:53:58 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+include 'db.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $query = $db->query("SELECT * FROM users WHERE email='$email'");
+    $user = $query->fetchArray(SQLITE3_ASSOC);
+
+    if ($user && password_verify($password, $user['password'])) {
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['name'] = $user['name'];
+        header("Location: dashboard.php");
+    } else {
+        echo "Invalid Email or Password!";
+    }
+}
+?>
+  <form method="POST">
+    Email: <input type="email" name="email" required><br>
+    Password: <input type="password" name="password" required><br>
+    <button type="submit">Login</button>
+</form>
