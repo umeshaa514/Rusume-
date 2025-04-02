50f223498e8ed321f6a06ea74ff598cceffa126b
umeshaa514 <ur4190150@gmail.com>
2025-04-02 22:45:23 +0530

    Create register.php

---
+++ register.php
@@ -0,0 +1,6 @@
+<form method="POST">
+    Name: <input type="text" name="name" required><br>
+    Email: <input type="email" name="email" required><br>
+    Password: <input type="password" name="password" required><br>
+    <button type="submit">Register</button>
+</form>
