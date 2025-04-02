fd37d5ab7ca697fd32a68e38cfee573e8dd8afb9
umeshaa514 <ur4190150@gmail.com>
2025-04-02 22:50:56 +0530

    Update register.php

--- register.php
+++ register.php
@@ -14,10 +14,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "Error: " . $db->lastErrorMsg();
     }
 }
-?>
+
 <form method="POST">
     Name: <input type="text" name="name" required><br>
     Email: <input type="email" name="email" required><br>
     Password: <input type="password" name="password" required><br>
     <button type="submit">Register</button>
 </form>
+?>
